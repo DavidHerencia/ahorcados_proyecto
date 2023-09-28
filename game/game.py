9ccb709ad67802e6e6849ddfdfb2c5a46ec64174
Melanie Cortez Rojas <melaniecortezrojas@Melanies-MacBook-Pro.local>
2023-09-28 18:59:46 -0500

    link rds

--- game/game.py
+++ game/game.py
@@ -1,13 +1,15 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import DateTime, func
 from dataclasses import dataclass
 from flask_cors import CORS
+from datetime import datetime
 
 # config
 app = Flask(__name__)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:utec@localhost/ahorcados_db'  #TO CHANGE
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:Co7LUH7WtFeL8FaAcA5B@db-parcial.crxiuk7halti.us-east-1.rds.amazonaws.com:3306/ahorcados'  #TO CHANGE
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -37,7 +39,7 @@ class Game(db.Model):
     guesses1 = db.Column(db.String(20), nullable=False, default='')
     guesses2 = db.Column(db.String(20), nullable=False, default='')
     outcome = db.Column(db.Integer, nullable=False, default=0)
-    date = db.Column(db.String(20), nullable=False, default=f'{datetime.datetime.now()}')
+    date = db.Column(DateTime, nullable=False, default=datetime.now())
 
     def __repr__(self):
         return f'<game {self.id}>'
