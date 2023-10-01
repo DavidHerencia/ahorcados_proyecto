d1ef5ce8f8876946d09c0db37557aaff4ce7e3ad
DavidHerencia <david.herencia@utec.edu.pe>
2023-10-01 14:33:13 -0500

    frontend->backend connection correction

--- frontend/src/components/config.js
+++ frontend/src/components/config.js
@@ -1,3 +1,3 @@
-const loadbalancer = "http://bl-ahorcados-1075366561.us-east-1.elb.amazonaws.com/";
+const loadbalancer = "http://bl-ahorcados-1075366561.us-east-1.elb.amazonaws.com";
 
 export default loadbalancer;
